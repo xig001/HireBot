44f2c706fe047f1f95cfdd6e0fe1ce095442c885
xig001 <138840152+xig001@users.noreply.github.com>
2025-04-09 20:03:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -105,9 +105,7 @@ if uploaded_file and submit_button:
 
     with st.spinner("Analyzing resume..."):
         top_jobs, sim_scores, sal_scores, comb_scores = recommend_jobs(resume_path, job_df, glove_embeddings)
-        top_jobs["job_id"] = top_jobs["job_id"].astype(str)
-        job_details_df["job_id"] = job_details_df["job_id"].astype(str)
-        top_jobs = top_jobs.merge(job_details_df, on="job_id", how="left")
+        
         if "job_id" not in job_details_df.columns:
             job_details_df.rename(columns={job_details_df.columns[0]: "job_id"}, inplace=True)
 
