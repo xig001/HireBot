593bf2a07319986a900bbb73791bb92de80797fb
xig001 <138840152+xig001@users.noreply.github.com>
2025-04-09 20:46:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ import pickle
 
 from model import recommend_jobs
 from resume_parser import parse_pdf, extract_resume_info, vectorize_text_glove, load_glove_embeddings
-job_meta_url = "https://drive.google.com/uc?id=1UbRU1DEwXAu456CC4C_frmNye51gYuAz"
-job_details_df = pd.read_csv(job_meta_url)
-job_details_df.columns = job_details_df.columns.str.strip()
+job_details_url = "https://drive.google.com/uc?id=1UbRU1DEwXAu456CC4C_frmNye51gYuAz"
+job_details_path = "job_details.csv"
+gdown.download(job_details_url, job_details_path, quiet=False)
+job_details_df = pd.read_csv(job_details_path)
 
 st.set_page_config(page_title="HireBot – Job Recommendation System", layout="centered")
 
